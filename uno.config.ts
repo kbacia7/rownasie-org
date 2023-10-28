a8ba579af5e550a87b659bd6336574ddc6946c63
Kamil Bacia <kbacia7@gmail.com>
2023-10-28 11:56:59 +0200

    chore: removed @unocss/preset-attributify

--- uno.config.ts
+++ uno.config.ts
@@ -1,13 +1,11 @@
 // uno.config.ts
 import { defineConfig } from "unocss";
 import presetUno from "@unocss/preset-uno";
-import presetAttributify from "@unocss/preset-attributify";
 import presetWebFonts from "@unocss/preset-web-fonts";
 
 export default defineConfig({
   presets: [
     presetUno(),
-    presetAttributify(),
     presetWebFonts({
       provider: "google",
       fonts: {
