74c9f43d170ed389d26b2d3afc65aeee0a0dc968
Kamil Bacia <kbacia7@gmail.com>
2023-11-28 18:56:00 +0100

    fix(getAssociationInfoEntry): removed unnecessary extra await

--- src/helpers/collections/getAssociationInfoEntry.ts
+++ src/helpers/collections/getAssociationInfoEntry.ts
@@ -3,4 +3,4 @@ import { getEntry } from "astro:content";
 import type { Language } from "@i18n/languages";
 
 export default async (language: Language) =>
-  await await getEntry("association-info", `${language}/main`);
+  await getEntry("association-info", `${language}/main`);
